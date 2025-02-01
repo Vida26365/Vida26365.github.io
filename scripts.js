a7bb0c32b4f9fbd05618e82604ad90306a636035
vida26365 <vm36265@student.uni-lj.si>
2025-02-01 23:22:10 +0100

    Added dinamic elements

---
+++ scripts.js
@@ -0,0 +1,26 @@
+json_path = "remarkable/metadata.json";
+
+async function fetchFiles() {
+    const response = await fetch(json_path);
+    const slovar = await response.json();
+    const fileList = document.getElementById('file-list');
+
+    function for_files(dict, path = '') {
+        for (const folder in dict.folders) {
+            const folderElement = document.createElement("div");
+            const folderPath = `${path}${folder}/`;
+            for_files(dict.folders[folder].content, folderPath);
+        }
+        for (const file in dict.files) {
+            const fileElement = document.createElement("div");
+            const linkElement = document.createElement("a");
+            linkElement.textContent = file;
+            linkElement.href = `${path}${file}`;
+            fileElement.appendChild(linkElement);
+            fileList.appendChild(fileElement);
+        }
+    }
+
+}
+
+fetchFiles();
